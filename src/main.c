11c6360937a1bf221af6d239aed89a61a6b501c8
James Smith <james@theta.pw>
2014-10-09 07:30:01 -0500

    Add GPL notice to header of each file.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,19 @@
-/* Copyright (c) 2014 James Smith <james@theta.pw>
- * See the LICENSE file for your rights
+/*
+ * Copyright (C) 2014  James Smith <james@theta.pw>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 /* Non-standard BSD extensions */
@@ -40,10 +54,22 @@ static void cleanup(void)
 
 static void usage(void)
 {
+
+	const char *LICENSE =
+"Copyright (C) 2014 James Smith <james@theta.pw>\n\n"
+"This program is free software; you can redistribute it and/or modify\n"
+"it under the terms of the GNU General Public License as published by\n"
+"the Free Software Foundation; either version 2 of the License, or\n"
+"(at your option) any later version.\n\n"
+"This program is distributed in the hope that it will be useful,\n"
+"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+"GNU General Public License for more details.\n";
+
 	extern const char *__progname;
-	fprintf(stderr, "Copyright (C) 2014 James Smith <james@theta.pw>\n"
-		"See the license.txt file included with the release\n\n"
-		"%s-%s usage:\n\t" "[-h] this help\n", __progname, VERSION);
+	fprintf(stderr, "%s\nBuilt on %s at %s\n"
+		"%s-%s usage:\n\t" "[-h] this help\n",
+		LICENSE, __DATE__, __TIME__, __progname, VERSION);
 
 	exit(EXIT_FAILURE);
 }
