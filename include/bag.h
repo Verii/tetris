b9c9952b39116c106a009171f151680edfe507af
James Smith <james@theta.pw>
2014-10-26 20:15:10 -0500

    Fixed copyright headers.

--- include/bag.h
+++ include/bag.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) <year>  <name of author>
+ * Copyright (C) 2014  James Smith <james@theta.pw>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
